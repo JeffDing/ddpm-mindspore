fd897c13f0d2911d4e79e25070832bc1954ed139
jeffding <jeffding890430@163.com>
2023-03-24 09:05:31 +0800

    change ddpm baseline

--- ddm/ema.py
+++ ddm/ema.py
@@ -1,37 +1,31 @@
+import copy
 import mindspore
 from mindspore import Tensor, Parameter, ParameterTuple, ms_function
 from mindspore import nn, ops
 
-import numpy as np
-
-
-# same
 def exists(val):
     return val is not None
 
-
-# same
-def clamp(value, min_value=None, max_value=None):
-    # if exists(min_value):
-    #     value = ops.maximum(value, min_value)
-    #
-    # if exists(max_value):
-    #     value = ops.minimum(value, max_value)
-
+def clamp(value, min_value = None, max_value = None):
     if exists(min_value):
-        min_value = Tensor(np.array(min_value, shape=value.shape))
-        value = ops.Maximum(value, min_value)
+        value = ops.maximum(value, min_value)
 
     if exists(max_value):
-        max_value = Tensor(np.array(max_value, shape=value.shape))
         value = ops.minimum(value, max_value)
 
     return value
 
-
 class EMA(nn.Cell):
     """
     Implements exponential moving average shadowing for your model.
+    Utilizes an inverse decay schedule to manage longer term training runs.
+    By adjusting the power, you can control how fast EMA will ramp up to your specified beta.
+    @crowsonkb's notes on EMA Warmup:
+    If gamma=1 and power=1, implements a simple average. gamma=1, power=2/3 are
+    good values for models you plan to train for a million or more steps (reaches decay
+    factor 0.999 at 31.6K steps, 0.9999 at 1M steps), gamma=1, power=3/4 for models
+    you plan to train for less (reaches decay factor 0.999 at 10K steps, 0.9999 at
+    215.4k steps).
     Args:
         inv_gamma (float): Inverse multiplicative factor of EMA warmup. Default: 1.
         power (float): Exponential factor of EMA warmup. Default: 1.
@@ -40,13 +34,14 @@ class EMA(nn.Cell):
     def __init__(
         self,
         model,
-        beta=0.9999,
-        update_after_step=100,
-        update_every=10,
-        inv_gamma=1.0,
-        power=2 / 3,
-        min_value=0.0,
-        ignore_names=None,
+        beta = 0.9999,
+        update_after_step = 100,
+        update_every = 10,
+        inv_gamma = 1.0,
+        power = 2 / 3,
+        min_value = 0.0,
+        params_no_ema = set(),
+        ignore_names = set(),
     ):
         super().__init__()
         self.beta = beta
@@ -74,15 +69,15 @@ class EMA(nn.Cell):
         return success
 
     def get_current_decay(self):
-        epoch = clamp(self.step - self.update_after_step - 1, min_value=0.)
+        epoch = clamp(self.step - self.update_after_step - 1, min_value = 0.)
         value = 1 - (1 + epoch / self.inv_gamma) ** - self.power
 
         if epoch <= 0.:
             return Tensor(0., mindspore.float32)
 
-        return clamp(value, min_value=self.min_value, max_value=self.beta)
+        return clamp(value, min_value = self.min_value, max_value = self.beta)
 
-    # @ms_function
+    @ms_function
     def update(self):
         success = ops.assign_add(self.step, Tensor(1, mindspore.int32))
         if (self.step % self.update_every) != 0:
@@ -110,7 +105,7 @@ class EMA(nn.Cell):
         success = self.map(ops.partial(moving_average, current_decay), self.ema_params, self.online_params)
         return success
 
-    # @ms_function
+    @ms_function
     def synchronize(self):
         # model -> swap
         success = self.map(ops.assign, self.swap_params, self.online_params)
@@ -118,7 +113,7 @@ class EMA(nn.Cell):
         success = ops.depend(success, self.map(ops.assign, self.online_params, self.ema_params))
         return success
 
-    # @ms_function
+    @ms_function
     def desynchronize(self):
         # swap -> model
         success = self.map(ops.assign, self.online_params, self.swap_params)
